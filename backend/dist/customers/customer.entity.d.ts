a8ac156389414db3b52ddc537ca3d8cbabf66727
Abbos <abbosqochqorboyev3@gmail.com>
2026-05-04 18:05:26 +0500

    replace frontend

--- backend/dist/customers/customer.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Customer {
-    id: string;
-    name: string;
-    contact_email: string;
-    phone: string;
-    address: string;
-    is_active: boolean;
-    created_at: Date;
-}
--- backend/dist/dispatchers/dispatcher.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Dispatcher {
-    id: string;
-    name: string;
-    team: string;
-    region: string;
-    user_id: string;
-    is_active: boolean;
-    created_at: Date;
-}
